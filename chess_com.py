de497d89af41d211eccf1e274029dae3a728f2d3
CodyReeth <55895840+CodyReeth@users.noreply.github.com>
2024-11-16 14:54:46 -0500

    Update chess_com.py

--- chess_com.py
+++ chess_com.py
@@ -1,23 +1,33 @@
 import requests
+import os
+import json
 
 
-#def fetch_games(username, year, month):
-    #url = f"https://api.chess.com/pub/player/{username}/games/{year}/{month}"
-url = f"https://api.chess.com/pub/player/magnuscarlsen/games/2014/12"
+def fetch_games(username, year, month):
+    output_dir = "chess_games"
+    url = f"https://api.chess.com/pub/player/{username}/games/{year}/{month}"
+    # User-Agent for Querying Chess.com
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Safari/537.36"
+    }
+    # Make the API request with headers
+    response = requests.get(url, headers=headers)
+    # Check the response
+    if response.status_code == 200:
+       print("Succesfully fetched data")
+       games = response.json()
+       _, year, month = url.split("/")[-3:]
+       filename = f"{year}-{month}.json"
+       filepath = os.path.join(output_dir, filename)
+       with open(filepath, "w") as file:
+           json.dump(games, file, indent=4)
+           print(f"Downloaded games saved to {filepath}")
+    else:
+        print(f"Failed to fetch games from {username}. Status code: {response.status_code}")
 
-# User-Agent for Querying Chess.com
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_0_0) AppleWebKit/537.36 (KHTML, like Gecko) Version/14.0 Safari/537.36"
-}
 
-# Make the API request with headers
-response = requests.get(url, headers=headers)
 
-# Check the response
-if response.status_code == 200:
-    print("Successfully fetched data:")
-    print(response.json())  # Display the JSON response
-else:
-    print(f"Failed to fetch data. Status code: {response.status_code}")
-    print(response.text)  # Display error details
+def main():
+    fetch_games("magnuscarlsen","2014","12")
 
+main()
